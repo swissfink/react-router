294303acbd8c20c5d5eafbda13427e83700ff069
Richard Fink <38076620+swissfink@users.noreply.github.com>
2019-06-04 15:21:54 -0400

    how about one more?

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -5,7 +5,7 @@ const db = require("../models");
 
 mongoose.connect(
   process.env.MONGODB_URI ||
-  "mongodb://localhost/reactreadinglist"
+  "mongodb://127.0.0.1/reactreadinglist"
 );
 
 const bookSeed = [
